10dacd4bd83728e5b7a990efe06035674e3c360d
20191844339 <2316423123@qq.com>
2019-10-21 17:20:24 +0800

    cly31

--- cly31/StdAfx.cpp
+++ cly31/StdAfx.cpp
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+// stdafx.cpp : source file that includes just the standard includes
+//	31m.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
+=======
 // stdafx.cpp : source file that includes just the standard includes
 //	cly3 1.pch will be the pre-compiled header
 //	stdafx.obj will contain the pre-compiled type information
@@ -6,3 +16,4 @@
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
+>>>>>>> cly
--- cly31/StdAfx.h
+++ cly31/StdAfx.h
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__D0D41CAC_1763_49C7_A6FC_16A94C5DC4B2__INCLUDED_)
+#define AFX_STDAFX_H__D0D41CAC_1763_49C7_A6FC_16A94C5DC4B2__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__D0D41CAC_1763_49C7_A6FC_16A94C5DC4B2__INCLUDED_)
+=======
 // stdafx.h : include file for standard system include files,
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
@@ -20,3 +44,4 @@
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
 #endif // !defined(AFX_STDAFX_H__DE424365_501A_4D90_B052_B76DB748B568__INCLUDED_)
+>>>>>>> cly
